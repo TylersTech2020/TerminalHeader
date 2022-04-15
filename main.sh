aadd5a67b6ecdb41c239bfccdbe978a5d6be18bb
LawlessCarrot <76822021+TylersTech2020@users.noreply.github.com>
2022-04-14 20:42:48 -0400

    Create main.sh
    Bored

---
+++ main.sh
@@ -0,0 +1,19 @@
+#Terminal Header
+#figlet Matrix Magic | lolcat -a -d 50
+figlet -f Bloody Matrix | lolcat
+
+echo "     .       .    )        .           ."
+echo "   .       *             .         ."
+echo "               .                      ."
+echo "   .       .                   ."
+echo "                                *        ."
+echo "      .   '               .              ."
+echo "              _.---._   .            .     *"
+echo "    *       .'       '."
+echo "        _.-~===========~-._"
+echo "       (___________________)       .   *"
+echo "  __  .'     \_______/   .'  ______        __"
+echo "    |              .'  .'   |      |      |  |"
+echo "    |             '         |      |      |  |"
+echo "    |                       |      |   ___|  |_"
+echo "  __|_______________________|__..--~~~~    ~--."
